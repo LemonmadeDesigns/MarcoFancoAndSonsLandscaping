84c0a215ea2e0c4ecfc04473f603998f310aca8c
Terrell D Lemons <lemonsterrell2021@gmail.com>
2021-06-29 21:52:58 -0700

    Fixed Title and completed all pages

--- public/js/header.js
+++ public/js/header.js
@@ -5,11 +5,11 @@ anime
 		opacity: [0.5, 1],
 		scaleX: [0, 1],
 		easing: "easeInOutExpo",
-		duration: 700,
+		duration: 500,
 	})
 	.add({
 		targets: ".ml5 .line",
-		//  duration: 600,
+		duration: 600,
 		easing: "easeOutExpo",
 		translateY: (el, i) => -0.625 + 0.625 * 2 * i + "em",
 	})
@@ -18,7 +18,7 @@ anime
 		opacity: [0, 1],
 		scaleY: [0.5, 1],
 		easing: "easeOutExpo",
-		//  duration: 600,
+		duration: 600,
 		offset: "-=600",
 	})
 	.add({
@@ -26,7 +26,7 @@ anime
 		opacity: [0, 1],
 		translateX: ["0.5em", 0],
 		easing: "easeOutExpo",
-		//  duration: 600,
+		duration: 600,
 		offset: "-=300",
 	})
 	.add({
@@ -34,7 +34,7 @@ anime
 		opacity: [0, 1],
 		translateX: ["-0.5em", 0],
 		easing: "easeOutExpo",
-		//  duration: 600,
+		duration: 600,
 		offset: "-=600",
 	})
 	.add({
@@ -42,5 +42,6 @@ anime
 		opacity: 0,
 		duration: 1000,
 		easing: "easeOutExpo",
-		delay: 1000,
+		// DELAY IT FROM DISAPPEARING
+		delay: 5000,
 	});
